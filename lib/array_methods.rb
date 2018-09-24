09e3c8bbda970d714abd5db16c65ffdd997f71a8
Omid Vatandoost <omidvatan94@gmail.com>
2018-09-24 18:16:44 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,8 @@
 def using_include(array, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array.include?("wow")
+  array.include?("poop")
+end
 end
 
 def using_sort(array)
